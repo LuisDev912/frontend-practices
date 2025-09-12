2b7becfb4c88e692016a652c8ba977dcebbc5717
Luis Calleja <luiscallejaenrique@gmail.com>
2025-09-12 10:50:13 -0300

    "button-counter project ready - Select classes and IDs in the JS file. - Create a good design on the CSS file. - Create divs and buttons on the HTML file."

---
+++ button-counter/script.js
@@ -0,0 +1,9 @@
+//select the containers:
+const container = document.querySelector('.container')
+const numberContainer = document.querySelector('.buttons')
+//select the number and the buttons:
+const number = document.getElementById('number');
+const increase = document.getElementById('increase');
+const decrease = document.getElementById('decrease');
+const reset = document.getElementById('reset');
+
