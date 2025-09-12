1d2c6b914aca2608e07979178a51aff9d426cd28
Luis Calleja <luiscallejaenrique@gmail.com>
2025-09-12 11:07:03 -0300

    "Buttons now work (button-counter) - Create count variable that will change when a button is pressed. - Delete the variables that contain the divs because they were not used. - Add flex-wrap to the buttons div."

--- button-counter/script.js
+++ button-counter/script.js
@@ -1,9 +1,23 @@
-//select the containers:
-const container = document.querySelector('.container')
-const numberContainer = document.querySelector('.buttons')
+let count = 0;
 //select the number and the buttons:
 const number = document.getElementById('number');
 const increase = document.getElementById('increase');
 const decrease = document.getElementById('decrease');
 const reset = document.getElementById('reset');
 
+// button functions
+
+increase.addEventListener('click', () => {
+    count++;
+    number.textContent = count;
+});
+
+decrease.addEventListener('click', () => {
+    count--;
+    number.textContent = count;
+});
+
+reset.addEventListener('click', () => {
+    count = 0;
+    number.textContent = count;
+});
