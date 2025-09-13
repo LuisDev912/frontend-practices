2c8cba35ff9e34da431e3f3f7a86e282bab61c1d
Luis Calleja <luiscallejaenrique@gmail.com>
2025-09-13 12:26:29 -0300

    "TO-DO: Add new functionalities - Add tabindex in the HTML. - Prevents empty inputs in the JS file. - Add style to the text input. - Now the user can create a new task."

--- TO-DO/script.js
+++ TO-DO/script.js
@@ -1,8 +1,32 @@
 const container = document.querySelector('.container');
 const list = document.getElementById('list');
-const checkbox = document.getElementById('checkbox');
+const checkbox = document.querySelectorAll('.checkbox');
 const input = document.getElementById('elementInput');
+const item = document.getElementsByTagName('li');
 //buttons
 const addElements = document.getElementById('addElement');
 const deleteElements = document.getElementById('removeElements');
+const submit = document.getElementById('submit');
 
+addElements.addEventListener('click', () => {
+    input.style.display = "inline"
+    submit.style.display = "inline"
+    input.focus();
+});
+
+submit.addEventListener('click', () => {
+    if (input.value.trim() === "") return; 
+
+    const li = document.createElement('li');
+    const newCheckbox = document.createElement('input');
+    newCheckbox.type = 'checkbox';
+
+    li.appendChild(newCheckbox);
+    li.appendChild(document.createTextNode(input.value));
+
+    list.appendChild(li);
+    input.value = '';
+
+    input.style.display = "none"
+    submit.style.display = "none"
+});
