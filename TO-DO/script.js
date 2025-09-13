54b4b63277f760a16599b58f717a9be5bab10239
Luis Calleja <luiscallejaenrique@gmail.com>
2025-09-13 11:30:06 -0300

    "TO-DO: Project ready to add JS - Add button style from uiverse. - Create an input that will apear when the user click the first button. - Select the buttons and the input in the JS file."

--- TO-DO/script.js
+++ TO-DO/script.js
@@ -1,3 +1,8 @@
 const container = document.querySelector('.container');
 const list = document.getElementById('list');
 const checkbox = document.getElementById('checkbox');
+const input = document.getElementById('elementInput');
+//buttons
+const addElements = document.getElementById('addElement');
+const deleteElements = document.getElementById('removeElements');
+
