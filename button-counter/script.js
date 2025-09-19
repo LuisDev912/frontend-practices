b14bc6db006f2b00f629e2cc0efe42ebd7d8e2f8
Luis Calleja <luiscallejaenrique@gmail.com>
2025-09-19 17:05:10 -0300

    "button-counter: Improve readability - Simplify the JS code with 3 variables and a foreach loop."

--- button-counter/script.js
+++ button-counter/script.js
@@ -1,23 +1,15 @@
 let count = 0;
 //select the number and the buttons:
 const number = document.getElementById('number');
-const increase = document.getElementById('increase');
-const decrease = document.getElementById('decrease');
-const reset = document.getElementById('reset');
+const buttons = document.querySelectorAll('#increase, #decrease, #reset');
 
 // button functions
-
-increase.addEventListener('click', () => {
-    count++;
-    number.textContent = count;
-});
-
-decrease.addEventListener('click', () => {
-    count--;
-    number.textContent = count;
-});
-
-reset.addEventListener('click', () => {
-    count = 0;
-    number.textContent = count;
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (btn.id == "increase") { count++ }
+        if (btn.id == "decrease") { count-- }
+        if (btn.id == "reset") { count = 0 }
+        number.textContent = count;
+    });
+    
 });
