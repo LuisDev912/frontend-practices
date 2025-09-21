d23db2d221306babee580bfd99856b147e27665f
Luis Calleja <luiscallejaenrique@gmail.com>
2025-09-21 12:11:03 -0300

    "card-generator: Implement JavaScript to handle the form - Create two variables to limit the MB size of the photo. - Add an event listener to handle submission."

---
+++ card-generator/scripts.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('cardForm');
+const MAX_SIZE_MB = 1; // 1 MB
+const MAX_SIZE_BYTES = MAX_SIZE_MB * 1024 * 1024;
+
+form.addEventListener('submit', (e) => {
+    e.defaultPrevented();
+
+    const title = document.getElementById('title').value;
+    const photoInput = document.getElementById('photoSelector');
+    const subtitle = document.getElementById('subtitle').value;
+    const description = document.getElementById('description').value;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+        const photo = reader.result;
+
+        const card = {
+            title,
+            photo,
+            subtitle,
+            description
+        };
+
+        //I'll use localStorage to store the card data temporarily, but later
+    };
+});
