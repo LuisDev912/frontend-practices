19d4bb268ab582319e4bc77dcbb6394a7322e853
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-10 19:17:24 -0300

    rock-paper-scissors: Add comments

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -12,17 +12,20 @@ const computerChoiceElement = document.getElementById('computer-choice');
 const result = document.getElementById('result');
 const comparing = document.getElementById('comparing-choice');
 
+// function when a button is pressed
+
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const userChoice = button.dataset.choice;
         const computerChoice = Object.keys(rules)[Math.floor(Math.random() * 3)];
-        button.classList.toggle('disabled');
+        button.classList.toggle('disabled'); // actives .disabled class
 
         userChoiceElement.textContent = userChoice;
         computerChoiceElement.textContent = '---';
         result.textContent = " ";
         comparing.style.display = 'inline';
 
+        // setTimeout is used to do something after a specific delay
         setTimeout(() => {
             comparing.style.display = 'none';
             computerChoiceElement.textContent = computerChoice;
@@ -35,7 +38,7 @@ buttons.forEach(button => {
                 result.textContent = "You lost...";
             }
 
-            button.classList.toggle('disabled');
+            button.classList.toggle('disabled'); // disables .disabled class
         }, 1000);
     });
 });
