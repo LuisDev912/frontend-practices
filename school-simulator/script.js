a2b089a0a19766c891abf47c386dde904932213e
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-12 16:39:42 -0300

    school-simulator: Add functionality to add new students

--- school-simulator/script.js
+++ school-simulator/script.js
@@ -1,11 +1,25 @@
 const nameInp = document.getElementById('name-insert');
 const gradeInp = document.getElementById('grade-insert');
 const addBtn = document.getElementById('submit');
+const table = document.querySelector('.tbody');
 
 addBtn.addEventListener('click', (event) => {
     event.preventDefault();
 
-    const fullName = nameInp.value;
-    const grades = gradeInp.value;
-    console.log(fullName, grades); // only for test purpose
+    const newStudent = {
+        name: nameInp.value,
+        grade: Number(gradeInp.value),
+        attendance: "Pending",
+        warnings: 0
+    };
+
+    const newTable = document.createElement('tr');
+    newTable.innerHTML = `
+        <td>${newStudent.name}</td>
+        <td>${newStudent.attendance}</td>
+        <td>${newStudent.grade}</td>
+        <td class="Warnings">${newStudent.warnings}</td>
+    `;
+
+    table.appendChild(newTable);
 });
