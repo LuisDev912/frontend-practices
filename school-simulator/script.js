7683ce948b5d877631b38af4e44e2e41ae30f9ff
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-12 17:19:18 -0300

    school-simulator: Add encapsulation to improve readability

--- school-simulator/script.js
+++ school-simulator/script.js
@@ -3,32 +3,36 @@ const gradeInp = document.getElementById('grade-insert');
 const addBtn = document.getElementById('submit');
 const table = document.querySelector('.tbody');
 
-addBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+function newTable(student) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>${student.name}</td>
+        <td>${student.attendance}</td>
+        <td>${student.grade}</td>
+        <td class="Warnings">${student.warnings}</td>
+    `;
+    return row;
+}
 
-    if (!nameInp.form.reportValidity()) {
-        return;
-    }
+function studentApp(){
+    addBtn.addEventListener('click', (event) => {
+        event.preventDefault();
 
-    if (nameInp.value !== "" && !isNaN(gradeInp.value) && parseInt(gradeInp.value) < 10){
-        const newStudent = {
-            name: nameInp.value,
-            grade: Number(gradeInp.value),
-            attendance: "Pending",
-            warnings: 0
-        };
+        if (!nameInp.form.reportValidity()) return;
 
-        const newTable = document.createElement('tr');
-        newTable.innerHTML = `
-            <td>${newStudent.name}</td>
-            <td>${newStudent.attendance}</td>
-            <td>${newStudent.grade}</td>
-            <td class="Warnings">${newStudent.warnings}</td>
-        `;
+        if (nameInp.value !== "" && !isNaN(gradeInp.value) && parseInt(gradeInp.value) < 10){
+            const newStudent = {
+                name: nameInp.value,
+                grade: Number(gradeInp.value),
+                attendance: "Pending",
+                warnings: 0
+            };
 
-        table.appendChild(newTable);
+            const newRow = newTable(newStudent);
+            table.appendChild(newRow);
 
-        nameInp.value = ""; // clears inputs
-        gradeInp.value = "";
-    }
-});
+            nameInp.value = ""; // clears inputs
+            gradeInp.value = "";
+        }
+    });
+}
