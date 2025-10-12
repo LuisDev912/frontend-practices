bdd7e44e500dc4acb6fc7e0884bcb14c427430bf
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-12 16:50:16 -0300

    "school-simulator: Add validation to avoid empty inputs - Clear inputs after adding a student."

--- school-simulator/script.js
+++ school-simulator/script.js
@@ -6,20 +6,25 @@ const table = document.querySelector('.tbody');
 addBtn.addEventListener('click', (event) => {
     event.preventDefault();
 
-    const newStudent = {
-        name: nameInp.value,
-        grade: Number(gradeInp.value),
-        attendance: "Pending",
-        warnings: 0
-    };
+    if (nameInp.value !== "" && !isNaN(gradeInp.value)){
+        const newStudent = {
+            name: nameInp.value,
+            grade: Number(gradeInp.value),
+            attendance: "Pending",
+            warnings: 0
+        };
 
-    const newTable = document.createElement('tr');
-    newTable.innerHTML = `
-        <td>${newStudent.name}</td>
-        <td>${newStudent.attendance}</td>
-        <td>${newStudent.grade}</td>
-        <td class="Warnings">${newStudent.warnings}</td>
-    `;
+        const newTable = document.createElement('tr');
+        newTable.innerHTML = `
+            <td>${newStudent.name}</td>
+            <td>${newStudent.attendance}</td>
+            <td>${newStudent.grade}</td>
+            <td class="Warnings">${newStudent.warnings}</td>
+        `;
 
-    table.appendChild(newTable);
+        table.appendChild(newTable);
+
+        nameInp.value = ""; // clears inputs
+        gradeInp.value = "";
+    }
 });
