7642f9bc0211bcc4f60ba209d1f3af8601d07712
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-24 19:32:48 -0300

    usd-to-ars: set currency values

--- usd-to-ars/script.js
+++ usd-to-ars/script.js
@@ -3,14 +3,19 @@
 let usdToArs;
 let arsToUsd;
 
+// get values with fetch
 fetch('https://dolarapi.com/v1/dolares/oficial')
     .then(res => {
         if (!res.ok) throw new Error(`HTTP error: ${res.status}`);
         return res.json();
     })
     .then((data) => {
-        console.log(data)
+        usdToArs = data.compra;
+        arsToUsd = 1 / usdToArs;
+        console.log(`1 USD = ${arsToUsd} ARS`)
     })
     .catch((err) => {
-        alert(`Error detected: ${err}`);
+        alert(err);
+        usdToArs = 1000; // approximate
+        arsToUsd = 1 / usdToArs;
     });
