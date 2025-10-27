83c0a9813abad28ce5cf5bbd82cc8e88b724701b
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-27 16:42:05 -0300

    react-counter: Create buttons and display counter

--- react-counter/src/counter.jsx
+++ react-counter/src/counter.jsx
@@ -2,12 +2,17 @@ import { use, useState } from "react";
 
 function Counter() {
     const [count, setCount] = useState(0);
+    const increment = () => setCount(prev => prev + 1);
+    const decrement = () => setCount(prev => prev - 1);
+    const reset = () => setCount(0);
 
     return (
         <div className='box'>
-            <h2>The buttons will be displayed here</h2>
+            <h2>{ count }</h2>
             <div className='buttons'>
-                <button>button example</button>
+                <button onClick={increment}>increment</button>
+                <button onClick={decrement}>Decrement</button>
+                <button onClick={reset}>Reset</button>
             </div>
         </div>
     )
