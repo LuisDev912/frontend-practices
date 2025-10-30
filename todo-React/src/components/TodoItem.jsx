357a453363d5be05aadfd03dd8437d420702d9e5
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-30 16:34:50 -0300

    todo-react: add line-through effect when a task is done

--- todo-React/src/components/TodoItem.jsx
+++ todo-React/src/components/TodoItem.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const TodoItem = ({ task }) => {
+    const [isChecked, setChecked] = useState(false);
+
+    const textStyle = {
+        textDecoration: isChecked ? 'line-through' : 'none',
+        textDecorationColor: isChecked ? 'gray' : 'inherit',
+    };
+
+    function changeCheckbox() {
+        setChecked(!isChecked);
+    }
+
     return (
         <li>
-            <input type="checkbox" id={`task-${task}`} />
-            <label htmlFor={`task-$task`}>{task}</label>
+            <input type="checkbox" id={`task-${task}`} checked={isChecked} onChange={changeCheckbox}/>
+            <label style={textStyle} htmlFor={`task-$task`}>{task}</label>
         </li>
     );
 };
