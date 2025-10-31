e697ed4eb9e905338b53d401041b8c82d446abdb
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-31 17:53:50 -0300

    todo-react: create handleSubmit function with preventDefault

--- todo-React/src/components/TodoForm.jsx
+++ todo-React/src/components/TodoForm.jsx
@@ -3,8 +3,12 @@ import { useState } from "react";
 const TodoForm = () => {
     const [newTask, setNewTask] = useState('');
 
+    function handleSubmit(event) {
+        event.preventDefault();
+    };
+
     return (
-        <form id="task form">
+        <form id="task form" onSubmit={handleSubmit}>
             <input type="text" placeholder="enter a new task" />
             <input type="submit" value="send" />
         </form>
