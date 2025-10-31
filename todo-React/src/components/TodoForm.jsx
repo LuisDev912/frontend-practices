03b267a813f97b1457151488dfe8ed7e43de40aa
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-31 17:59:44 -0300

    "todo-react: connect TodoForm with App with props and handleSubmit - Add onAddTask prop to send new tasks to App. - Use useState to manage controlled input. - Moved onSubmit event from input to form. - Clear input field after submitting a task."

--- todo-React/src/components/TodoForm.jsx
+++ todo-React/src/components/TodoForm.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 
-const TodoForm = () => {
+const TodoForm = ({onAddTask}) => {
     const [newTask, setNewTask] = useState('');
 
-    function handleSubmit(event) {
-        event.preventDefault();
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (newTask.trim() === '') return;
+        
+        onAddTask(newTask); // sends the task to app component
+        setNewTask('');
     };
 
     return (
-        <form id="task form" onSubmit={handleSubmit}>
-            <input type="text" placeholder="enter a new task" />
+        <form id="task-form" onSubmit={handleSubmit}>
+            <input type="text" placeholder="enter a new task" value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
             <input type="submit" value="send" />
         </form>
     );
