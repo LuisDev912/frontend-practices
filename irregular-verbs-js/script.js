94183672199489ed6374738c07a731e0b88c323b
Luis Calleja <luiscallejaenrique@gmail.com>
2025-11-08 17:42:01 -0300

    "irregular-verbs-js: implement functionality to show random verb - Select more HTML elements and create verbsArray and currentVerb variables. - verbsArray saves all the verbs that were loaded from the JSON file. - A new infinitive verb is chosen randomly."

--- irregular-verbs-js/script.js
+++ irregular-verbs-js/script.js
@@ -4,6 +4,12 @@ const verbsSection = document.querySelector('.verbs-section');
 const loadVerbsBtn = document.getElementById('load-verbs-btn');
 const newVerbBtn = document.getElementById('new-verb-button');
 const selectedVerb = document.getElementById('selected-verb');
+const checkBtn = document.getElementById('check-button');
+const answerInput = document.getElementById('user-answer');
+const resultText = document.getElementById('result-message');
+
+let verbsArray = [];
+let currentVerb = null;
 
 fetch('verbs.json')
     .then(res => {
@@ -11,24 +17,33 @@ fetch('verbs.json')
         return res.json();
     })
     .then((verbs) => {
+        verbsArray = verbs;
+
         verbs.forEach(verb => {
             const p = document.createElement('p');
             verbsContainer.appendChild(p);
-            p.textContent = `${verb.past} → ${verb.past}`;
+            p.textContent = `${verb.infinitive} → ${verb.past}`;
         });
     })
     .catch((err) => {
         console.error(err);
-    })
+    });
 
 loadVerbsBtn.addEventListener('click', () => {
     verbsContainer.classList.toggle('active');
 });
 
-newVerbBtn.addEventListener('click', () => { 
+newVerbBtn.addEventListener('click', () => {
+    resultText.textContent = ''; 
+    answerInput.value = ''; // clears the result and the answer input
+
+    const randomIndex = Math.floor(Math.random() * verbsArray.length);
+    currentVerb = verbsArray[randomIndex];
+    
     selectedVerb.textContent = `the verb is...`;
 
     setTimeout(() => {
-        selectedVerb.textContent = 'run';
+        selectedVerb.textContent = currentVerb.infinitive;
+        answerInput.focus();
     }, 1000);
-})
+});
